a439d003e9d6bf44b09af38c56ef55e128665076
Ihor Andrieiev <andr.igorrom@gmail.com>
2021-05-12 05:14:16 +0300

    timer

---
+++ main.js
@@ -0,0 +1,53 @@
+class CountdownTimer {
+    constructor({selector, targetDate}) {
+        this.selector = document.querySelector(selector);
+        this.targetDate = targetDate;
+
+        this.refs = {
+          days: this.selector.querySelector('[data-value="days"]'),
+          hours: this.selector.querySelector('[data-value="hours"]'),
+          mins: this.selector.querySelector('[data-value="mins"]'),
+          secs: this.selector.querySelector('[data-value="secs"]'),
+      };
+    };
+
+getTimeOpts = () => {
+    setInterval(() => {
+        const timeDiff = this.getDiffTime();
+        const timeOpts = this.getTime(timeDiff);
+        this.renderTimerData(timeOpts);
+        //console.log(timeOpts);
+    }, 1000)
+    };
+   
+    getDiffTime = () => this.targetDate - Date.now();
+    
+   getDays = (time) => Math.floor(time / (1000 * 60 * 60 * 24));
+   getHours = (time) => Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+   getMins = (time) => Math.floor((time % (1000 * 60 * 60)) / (1000 * 60));
+   getSecs = (time) => Math.floor((time % (1000 * 60)) / 1000);
+
+    getTime = (time) => {
+        return {
+            days: this.getDays(time),
+            hours: this.getHours (time),
+            mins: this.getMins(time),
+            secs: this.getSecs(time),
+        }
+    }
+    
+    renderTimerData = timeOpts => {
+        const { days, hours, mins, secs } = timeOpts;
+        this.refs.days.textContent = days;
+        this.refs.hours.textContent = hours < 10 ? `0${hours}` : hours;
+        this.refs.mins.textContent = mins < 10 ? `0${mins}` : mins;
+        this.refs.secs.textContent = secs < 10 ? `0${secs}` : secs;
+    }
+};
+
+const timer = new CountdownTimer({
+  selector: '#timer-1',
+  targetDate: new Date('Feb 19, 2022'),
+});
+
+timer.getTimeOpts();
